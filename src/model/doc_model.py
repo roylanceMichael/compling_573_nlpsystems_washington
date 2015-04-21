6af07753b3fc4c103970a9f08991588b65bd4789
Brandon Gahler <gahlerbj@gmail.com>
2015-04-20 20:46:48 -0700

    Fixed date parsing in doc_model. (cherry picked from commit 1232fff)

--- src/model/doc_model.py
+++ src/model/doc_model.py
@@ -3,7 +3,12 @@ import nltk.data
 from nltk.tree import Tree
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import *
-# from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+from collections import defaultdict
+import string
+from datetime import datetime
+
+import src.extract.document
 
 sentence_breaker = nltk.data.load('tokenizers/punkt/english.pickle')
 stemmer = PorterStemmer().stem
@@ -12,6 +17,8 @@ chunking_grammar = r"""
       {<NNP>+}                # chunk sequences of proper nouns
 """
 chunker = nltk.RegexpParser(chunking_grammar)
+
+stopWords = set(stopwords.words('english')) | set(string.punctuation)
 
 nltk_v2 = nltk.__version__[0] == '2'
 
@@ -98,7 +105,7 @@ class Chunk(list, ParentCompare):
             self.full, self.tag = tree
             list.__init__(self, [Word(self.full, self.tag, self, 0)])
 
-        # coreference will occur at this level
+        # coreference will occure at this level
 
     def __str__(self):
         return self.full
@@ -120,13 +127,76 @@ class Word(ParentCompare):
 class Doc_Model:
     def __init__(self, doc):
         self.docNo = doc.docNo
-        self.dateTime = doc.dateTime
+        timeFormat = "%Y-%m-%d"
+        if "/" in doc.dateTime:
+            timeFormat = "%m/%d/%Y"
+        colonCount = doc.dateTime.count(":")
+        if colonCount == 2:
+            timeFormat += " %H:%M:%S"
+        elif colonCount == 1:
+            timeFormat += " %H:%M"
+
+        self.dateTime = datetime.strptime( doc.dateTime.strip(), timeFormat )
 
         self.header = Text(doc.header, self, -4)
         self.slug = Text(doc.slug, self, -3)
         self.headline = Text(doc.headline, self, -2)
         self.trailer = Text(doc.trailer, self, -1)
         self.body = Text(doc.body, self, 0)
-        self.paragraphs = [Text(doc.paragraphs[x], self, x + 1) for x in range(len(doc.paragraphs))]
+        if len(doc.paragraphs) > 1:
+            self.paragraphs = [Text(doc.paragraphs[x], self, x + 1) for x in range(len(doc.paragraphs))]
+        else:
+            #some documents have only tabbed paragraphs instead of xml delimited
+            tab_split = ( x.strip() for x in doc.paragraphs[0].split("\t") )
+            tab_split = [ x for x in tab_split if len(x) > 0 ]
 
+            self.paragraphs = [Text(tab_split[x], self, x + 1) for x in range(len(tab_split))]
+
+        # compute the term frequencies of all words in paragraphs
+        # assuming we should use the quarry version of tf if we are using it
+        # to rank sentences
+        self.termFreq = defaultdict(lambda: 0.5)
+        for word in self.words():
+            if word.full not in stopWords:
+                self.termFreq[word.full] = 1 + self.termFreq.get(word.full, 0.0)
+
+        maxFreq = max(self.termFreq.items(), key = lambda x: x[1] )[1]
+        for w in self.termFreq.keys():
+            self.termFreq[w] = (self.termFreq[w] / maxFreq) + 0.5
+
+    def __lt__(self, other):
+        return isinstance(other, Doc_Model) and self.dateTime < other.dateTime
+
+    def __le__(self, other):
+        return isinstance(other, Doc_Model) and self.dateTime <= other.dateTime
+
+    def __gt__(self, other):
+        return not self <= other
+
+    def __ge__(self, other):
+        return not self < other
+
+
+    def sentences(self):
+        for p in self.paragraphs:
+            for s in p:
+                yield s
+
+    def chunks(self):
+        for s in self.sentences():
+            for c in s:
+                yield c
+
+    def words(self):
+        for c in self.chunks():
+            for w in c:
+                yield w
+
+
+class Cluster(list):
+    def __init__(self, doclist):
+        if isinstance(doclist[0], src.extract.document.Document):
+            list.__init__(self, sorted(Doc_Model(x) for x in doclist) )
+        else:
+            list.__init__(self, sorted(doclist))
 
