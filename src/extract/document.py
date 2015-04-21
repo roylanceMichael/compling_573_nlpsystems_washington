1331418ecc7d1ebd2bd46453107d008f3bd462a7
Mike Roylance <roylance.michael@gmail.com>
2015-04-21 15:36:25 -0600

    updated formatting

--- src/extract/document.py
+++ src/extract/document.py
@@ -13,30 +13,30 @@ textKey = "TEXT"
 
 
 class Document:
-	def __init__(self):
-		"""
-			Initilize the document
-		"""
-		self.docNo = ""
-		self.dateTime = ""
-		self.header = ""
-		self.docType = ""
-		self.slug = ""
-		self.headline = ""
-		self.trailer = ""
-		self.body = ""
-		self.paragraphs = []
+    def __init__(self):
+        """
+            Initilize the document
+        """
+        self.docNo = ""
+        self.dateTime = ""
+        self.header = ""
+        self.docType = ""
+        self.slug = ""
+        self.headline = ""
+        self.trailer = ""
+        self.body = ""
+        self.paragraphs = []
 
-	def __str__(self):
-		"""
-			simple tostring method, used for the developer to see what the object looks like (need to cleanse single quotes)
-		"""
+    def __str__(self):
+        """
+            simple tostring method, used for the developer to see what the object looks like (need to cleanse single quotes)
+        """
 
-		paragraphText = ""
-		for paragraph in self.paragraphs:
-			paragraphText += paragraph
+        paragraphText = ""
+        for paragraph in self.paragraphs:
+            paragraphText += paragraph
 
-		return """
+        return """
 		{
 			docNo: '%s',
 			dateTime: '%s',
@@ -49,224 +49,224 @@ class Document:
 		}
 		""" % (self.docNo, self.dateTime, self.header, self.slug, self.headline, self.trailer, self.body, paragraphText)
 
-	@staticmethod
-	def build(objectDictionary):
-		"""
-			build the document object given a dictionary with string keys and array of strings values: { 'key', [ 'first', 'second' ]}
-		"""
+    @staticmethod
+    def build(objectDictionary):
+        """
+            build the document object given a dictionary with string keys and array of strings values: { 'key', [ 'first', 'second' ]}
+        """
 
-		newDocument = Document()
+        newDocument = Document()
 
-		if docNoKey in objectDictionary:
-			for item in objectDictionary[docNoKey]:
-				newDocument.docNo += item
+        if docNoKey in objectDictionary:
+            for item in objectDictionary[docNoKey]:
+                newDocument.docNo += item
 
-		if docTypeKey in objectDictionary:
-			for item in objectDictionary[docTypeKey]:
-				newDocument.docType += item
+        if docTypeKey in objectDictionary:
+            for item in objectDictionary[docTypeKey]:
+                newDocument.docType += item
 
-		if dateTimeKey in objectDictionary:
-			for item in objectDictionary[dateTimeKey]:
-				newDocument.dateTime += item
+        if dateTimeKey in objectDictionary:
+            for item in objectDictionary[dateTimeKey]:
+                newDocument.dateTime += item
 
-		if headerKey in objectDictionary:
-			for item in objectDictionary[headerKey]:
-				newDocument.header += item
+        if headerKey in objectDictionary:
+            for item in objectDictionary[headerKey]:
+                newDocument.header += item
 
-		if slugKey in objectDictionary:
-			for item in objectDictionary[slugKey]:
-				newDocument.slug += item
+        if slugKey in objectDictionary:
+            for item in objectDictionary[slugKey]:
+                newDocument.slug += item
 
-		if headlineKey in objectDictionary:
-			for item in objectDictionary[headlineKey]:
-				newDocument.headline += item
+        if headlineKey in objectDictionary:
+            for item in objectDictionary[headlineKey]:
+                newDocument.headline += item
 
-		if trailerKey in objectDictionary:
-			for item in objectDictionary[trailerKey]:
-				newDocument.trailer += item
+        if trailerKey in objectDictionary:
+            for item in objectDictionary[trailerKey]:
+                newDocument.trailer += item
 
-		if pKey in objectDictionary:
-			for item in objectDictionary[pKey]:
-				newDocument.paragraphs.append(item)
-		elif textKey in objectDictionary:
-			for item in objectDictionary[textKey]:
-				newDocument.paragraphs.append(item)
+        if pKey in objectDictionary:
+            for item in objectDictionary[pKey]:
+                newDocument.paragraphs.append(item)
+        elif textKey in objectDictionary:
+            for item in objectDictionary[textKey]:
+                newDocument.paragraphs.append(item)
 
-		if bodyKey in objectDictionary:
-			for item in objectDictionary[bodyKey]:
-				newDocument.body += item
+        if bodyKey in objectDictionary:
+            for item in objectDictionary[bodyKey]:
+                newDocument.body += item
 
-		return newDocument
+        return newDocument
 
-	@staticmethod
-	def returnCharsFromDocument(filePath):
-		"""
-			return the characters from a document
-		"""
-		if os.path.isfile(filePath):
-			with open(filePath) as f:
-				while True:
-					c = f.read(1)
+    @staticmethod
+    def returnCharsFromDocument(filePath):
+        """
+            return the characters from a document
+        """
+        if os.path.isfile(filePath):
+            with open(filePath) as f:
+                while True:
+                    c = f.read(1)
 
-					if not c:
-						return
+                    if not c:
+                        return
 
-					yield c
+                    yield c
 
-	@staticmethod
-	def returnCharsFromString(largeString):
-		"""
-			return the characters from a string
-		"""
-		for char in largeString:
-			yield char
+    @staticmethod
+    def returnCharsFromString(largeString):
+        """
+            return the characters from a string
+        """
+        for char in largeString:
+            yield char
 
-	@staticmethod
-	def factoryFromIndexer(documentIndexer):
-		if documentIndexer == None:
-			return None
+    @staticmethod
+    def factoryFromIndexer(documentIndexer):
+        if documentIndexer == None:
+            return None
 
-		index = 0
-		actualString = ""
-		for char in Document.returnCharsFromDocument(documentIndexer.fileName):
-			if index >= documentIndexer.start and index <= documentIndexer.end:
-				actualString += char
-			elif index > documentIndexer.end:
-				return Document.factory(actualString)
+        index = 0
+        actualString = ""
+        for char in Document.returnCharsFromDocument(documentIndexer.fileName):
+            if index >= documentIndexer.start and index <= documentIndexer.end:
+                actualString += char
+            elif index > documentIndexer.end:
+                return Document.factory(actualString)
 
-			index += 1
+            index += 1
 
-	@staticmethod
-	def factoryForSpecificDocNo(input, docNo):
-		"""
-			build 1 document given an input and doc number
-		"""
-		charMethod = Document.returnCharsFromDocument
+    @staticmethod
+    def factoryForSpecificDocNo(input, docNo):
+        """
+            build 1 document given an input and doc number
+        """
+        charMethod = Document.returnCharsFromDocument
 
-		tagStack = []
-		currentTag = ""
-		currentObject = {}
+        tagStack = []
+        currentTag = ""
+        currentObject = {}
 
-		seenOpeningTag = False
-		seenClosingTag = False
-		seenClosingXml = False
-		workspace = ""
+        seenOpeningTag = False
+        seenClosingTag = False
+        seenClosingXml = False
+        workspace = ""
 
-		foundDocNo = False
+        foundDocNo = False
 
-		for c in charMethod(input):
-			if c == "<":
-				seenOpeningTag = True
-				tagStackLen = len(tagStack)
+        for c in charMethod(input):
+            if c == "<":
+                seenOpeningTag = True
+                tagStackLen = len(tagStack)
 
-				if tagStackLen > 0:
-					lastTag = tagStack[tagStackLen - 1]
-					# check if we found the docno
-					if lastTag == docNoKey and workspace.strip() == docNo:
-						foundDocNo = True
+                if tagStackLen > 0:
+                    lastTag = tagStack[tagStackLen - 1]
+                    # check if we found the docno
+                    if lastTag == docNoKey and workspace.strip() == docNo:
+                        foundDocNo = True
 
-					if lastTag in currentObject and foundDocNo:
-						currentObject[lastTag].append(workspace)
-					elif foundDocNo:
-						currentObject[lastTag] = [workspace]
+                    if lastTag in currentObject and foundDocNo:
+                        currentObject[lastTag].append(workspace)
+                    elif foundDocNo:
+                        currentObject[lastTag] = [workspace]
 
-				currentTag = ""
-				workspace = ""
-			elif c == "/" and seenOpeningTag:
-				seenClosingXml = True
-			elif c == ">" and seenOpeningTag:
-				# remove the last one
-				if seenClosingXml and len(tagStack) > 0:
-					tagStack.pop()
-				else:
-					tagStack.append(currentTag)
+                currentTag = ""
+                workspace = ""
+            elif c == "/" and seenOpeningTag:
+                seenClosingXml = True
+            elif c == ">" and seenOpeningTag:
+                # remove the last one
+                if seenClosingXml and len(tagStack) > 0:
+                    tagStack.pop()
+                else:
+                    tagStack.append(currentTag)
 
-				seenOpeningTag = False
-				seenClosingXml = False
-				seenClosingTag = False
-				workspace = ""
+                seenOpeningTag = False
+                seenClosingXml = False
+                seenClosingTag = False
+                workspace = ""
 
-				endedXmlDoc = len(tagStack) == 0
-				if endedXmlDoc and foundDocNo:
-					return Document.build(currentObject)
-				elif endedXmlDoc:
-					currentObject = {}
-			elif seenOpeningTag:
-				currentTag += c
-			else:
-				workspace += c
-		return None
+                endedXmlDoc = len(tagStack) == 0
+                if endedXmlDoc and foundDocNo:
+                    return Document.build(currentObject)
+                elif endedXmlDoc:
+                    currentObject = {}
+            elif seenOpeningTag:
+                currentTag += c
+            else:
+                workspace += c
+        return None
 
-	@staticmethod
-	def factory(input, isFile=False):
-		"""
-			build a single document given an input
-		"""
-		result = list(Document.factoryMultiple(input, isFile))
+    @staticmethod
+    def factory(input, isFile=False):
+        """
+            build a single document given an input
+        """
+        result = list(Document.factoryMultiple(input, isFile))
 
-		if len(result) > 0:
-			return result[0]
-		return None
+        if len(result) > 0:
+            return result[0]
+        return None
 
-	@staticmethod
-	def factoryMultiple(input, isFile=False, isSingle=True):
-		"""
-			build multiple documents given an input
-		"""
+    @staticmethod
+    def factoryMultiple(input, isFile=False, isSingle=True):
+        """
+            build multiple documents given an input
+        """
 
-		charMethod = Document.returnCharsFromString
+        charMethod = Document.returnCharsFromString
 
-		if isFile:
-			charMethod = Document.returnCharsFromDocument
+        if isFile:
+            charMethod = Document.returnCharsFromDocument
 
-		tagStack = []
-		currentTag = ""
-		currentObject = {}
+        tagStack = []
+        currentTag = ""
+        currentObject = {}
 
-		seenOpeningTag = False
-		seenClosingTag = False
-		seenClosingXml = False
-		workspace = ""
+        seenOpeningTag = False
+        seenClosingTag = False
+        seenClosingXml = False
+        workspace = ""
 
-		for c in charMethod(input):
-			if c == "<":
-				seenOpeningTag = True
+        for c in charMethod(input):
+            if c == "<":
+                seenOpeningTag = True
 
-				tagStackLen = len(tagStack)
+                tagStackLen = len(tagStack)
 
-				if tagStackLen > 0:
-					lastTag = tagStack[tagStackLen - 1]
-					if lastTag in currentObject:
-						currentObject[lastTag].append(workspace)
-					else:
-						currentObject[lastTag] = [workspace]
+                if tagStackLen > 0:
+                    lastTag = tagStack[tagStackLen - 1]
+                    if lastTag in currentObject:
+                        currentObject[lastTag].append(workspace)
+                    else:
+                        currentObject[lastTag] = [workspace]
 
-				currentTag = ""
-				workspace = ""
-			elif c == "/" and seenOpeningTag:
-				seenClosingXml = True
-			elif c == ">" and seenOpeningTag:
-				# remove the last one
-				if seenClosingXml and len(tagStack) > 0:
-					tagStack.pop()
-				else:
-					tagStack.append(currentTag)
+                currentTag = ""
+                workspace = ""
+            elif c == "/" and seenOpeningTag:
+                seenClosingXml = True
+            elif c == ">" and seenOpeningTag:
+                # remove the last one
+                if seenClosingXml and len(tagStack) > 0:
+                    tagStack.pop()
+                else:
+                    tagStack.append(currentTag)
 
-				seenOpeningTag = False
-				seenClosingXml = False
-				seenClosingTag = False
-				workspace = ""
+                seenOpeningTag = False
+                seenClosingXml = False
+                seenClosingTag = False
+                workspace = ""
 
-				if len(tagStack) == 0:
-					yield Document.build(currentObject)
+                if len(tagStack) == 0:
+                    yield Document.build(currentObject)
 
-					# don't want to compute more than we have to...
-					if isSingle:
-						break
+                    # don't want to compute more than we have to...
+                    if isSingle:
+                        break
 
-					currentObject = {}
+                    currentObject = {}
 
-			elif seenOpeningTag:
-				currentTag += c
-			else:
-				workspace += c
+            elif seenOpeningTag:
+                currentTag += c
+            else:
+                workspace += c
