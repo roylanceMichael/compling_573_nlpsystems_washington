8d61eff482a40f3e05872986da50a4133c59b62f
Mike Roylance <roylance.michael@gmail.com>
2015-05-12 19:33:42 -0600

    caching the model summaries in rouge (will clean this up after the run)

--- src/printExtractions.py
+++ src/printExtractions.py
@@ -1,7 +1,8 @@
 __author__ = 'mroylance'
 
 import os
-import sys
+import extract
+import extract.topicReader
 import pickle
 import operator
 import extractionclustering.sentence
@@ -19,6 +20,18 @@ meadCacheDir = "../cache/meadCache"
 rougeCacheDir = "../cache/rougeCache"
 
 rouge = RougeEvaluator("../ROUGE", "/opt/dropbox/14-15/573/Data/models/devtest", summaryOutputPath, modelSummaryCachePath, rougeCacheDir)
+
+topics = []
+for topic in extract.topicReader.Topic.factoryMultiple("../doc/Documents/devtest/GuidedSumm10_test_topics.xml"):
+	topics.append(topic)
+
+documentRepository = extract.documentRepository.DocumentRepository("/corpora/LDC/LDC02T31/", "/corpora/LDC/LDC08T25/data/", topics)
+
+# load the cached docs
+documentRepository.readFileIdDictionaryFromFileCache(documentCachePath)
+
+# cache the model summaries
+rouge.cacheModelSummaries(topics)
 
 ##############################################################
 # helper function for printing out buffers to files
