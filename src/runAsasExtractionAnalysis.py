4025b2bee640f0ae7a8955a93e2f1bd94d950e9f
Mike Roylance <roylance.michael@gmail.com>
2015-05-16 14:23:03 -0600

    just doing 3 sentences

--- src/runAsasExtractionAnalysis.py
+++ src/runAsasExtractionAnalysis.py
@@ -99,7 +99,7 @@ for fileName in os.listdir(cachePath):
 				score = compareSentence.distanceToOtherSentence(allSentences[otherUniqueSentenceId])
 				scoreDictionary[uniqueSentenceId] += score
 
-		maxSentences = 15
+		maxSentences = 3
 		sentenceIdx = 0
 		uniqueSummaries = {}
 		for tupleResult in sorted(scoreDictionary.items(), key=operator.itemgetter(1), reverse=True):
