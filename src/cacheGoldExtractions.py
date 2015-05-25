cca23d8db2fdd770f78ec29202753fd5c5baf816
Mike Roylance <roylance.michael@gmail.com>
2015-05-25 12:39:21 -0600

    added in asas gold cache

--- src/cacheGoldExtractions.py
+++ src/cacheGoldExtractions.py
@@ -26,8 +26,8 @@ idfCachePath = "../cache/idfCache"
 meadCacheDir = "../cache/meadCache"
 rougeCacheDir = "../cache/rougeCache"
 
-ss = attensity.semantic_server.SemanticServer()
-configUrl = ss.configurations().config_url(7)
+ss = attensity.semantic_server.SemanticServer("http://192.168.1.11:8888")
+configUrl = ss.configurations().config_url(3)
 
 directories = ["/opt/dropbox/14-15/573/Data/models/devtest","/opt/dropbox/14-15/573/Data/models/training/2009","/opt/dropbox/14-15/573/Data/mydata"]
 
@@ -38,89 +38,66 @@ for directory in directories:
 		fileToPickle = os.path.join(directory, file)
 		text = ""
 		with open(fileToPickle, "r") as myFile:
-			text = unicode(myFile.read(), errors='replace')
+			text = unicode(myFile.read(), errors='ignore')
 
-		parse = ss.parse()
-		parse.process(text, configUrl)
-		ext = attensity.extractions.Extractions.from_protobuf(parse.result)
-		topicSummary = extractionclustering.topicSummary.TopicSummary()
-		topicSummary.text = text
+		rawDocument = extract.document.Document()
+		rawDocument.paragraphs.append(text)
+		initialModel = model.doc_model.Doc_Model(rawDocument)
+		coreference.rules.updateDocumentWithCoreferences(initialModel)
+		coherence.scorer.determineDoc(initialModel)
 
-		newParagraph = extractionclustering.paragraph.Paragraph()
-		newParagraph.text = text
-		for extraction in ext.extractions():
-			if extraction.type == attensity.ExtractionMessage_pb2.Extraction.KEYWORD_RESULTS:
-				roots = {}
-				for item in extraction.keyword_results.root:
-					roots[item.id] = {"root": item.root, "word": item.word, "pos": item.pos}
-				for item in extraction.keyword_results.location:
-					roots[item.id]["sentence"] = item.sentence
-				for key in roots:
-					if "sentence" not in roots[key]:
-						continue
+		docModel = extractionclustering.docModel.DocModel()
+		for paragraph in initialModel.paragraphs:
+			newParagraph = extractionclustering.paragraph.Paragraph()
+			docModel.paragraphs.append(newParagraph)
+			# cleansedParagraph = re.sub("\s+", " ", str(paragraph))
+			newParagraph.text = str(paragraph)
 
-					try:
-						sentenceId = int(roots[key]["sentence"])
-						root = str(roots[key]["root"])
-						word = str(roots[key]["word"])
-						pos = list(roots[key]["pos"])
-						newParagraph.extractionKeywordResults.append((sentenceId, root, word, pos))
-					except Exception:
-						print "error happened"
-			if extraction.type == attensity.ExtractionMessage_pb2.Extraction.FACT_RELATION:
-				newParagraph.extractionFactRelations.append((extraction.fact_relation.fact_one, extraction.fact_relation.fact_two, extraction.fact_relation.text))
-			if extraction.type == attensity.ExtractionMessage_pb2.Extraction.TEXT_SENTENCE:
-				print "|||" + newParagraph.text[extraction.text_sentence.offset:extraction.text_sentence.offset + extraction.text_sentence.length] + "|||"
-				newParagraph.extractionSentences.append((extraction.text_sentence.text_sentence_ID, extraction.text_sentence.offset, extraction.text_sentence.length))
-			if extraction.type == attensity.ExtractionMessage_pb2.Extraction.ENTITY:
-				mid = ""
-				if len(extraction.entity.search_info) > 0:
-					mid = extraction.entity.search_info[0].machine_ID
-				newParagraph.extractionEntities.append((extraction.entity.sentence_id, extraction.entity.display_text, extraction.entity.sem_tags, extraction.entity.domain_role, mid))
-			if extraction.type == attensity.ExtractionMessage_pb2.Extraction.TRIPLE:
-				newParagraph.extractionTriples.append((extraction.triple.sentence_ID, extraction.triple.t1.value, extraction.triple.t1.sem_tags, extraction.triple.t2.value, extraction.triple.t2.sem_tags, extraction.triple.t3.value, extraction.triple.t3.sem_tags))
-				# print extraction
-			if extraction.type == attensity.ExtractionMessage_pb2.Extraction.FACT:
-				# print extraction
-				newParagraph.extractionFacts.append((extraction.fact.sentence_ID, extraction.fact.element.text, extraction.fact.mode.text))
-			if extraction.type == attensity.ExtractionMessage_pb2.Extraction.TEXT_PHRASE:
-				# print extraction
-				newParagraph.extractionTextPhrases.append((extraction.text_phrase.sentence_ID, extraction.text_phrase.head, extraction.text_phrase.root))
+			parse = ss.parse()
+			parse.process(str(paragraph), configUrl)
+			ext = attensity.extractions.Extractions.from_protobuf(parse.result)
 
-		sentences = {}
-		sentenceNum = 0
-		for sentence in newParagraph.extractionSentences:
-			text = newParagraph.text
-			actualSentence = text[sentence[1]:sentence[1] + sentence[2]]
-			sentences[sentence[0]] = \
-				extractionclustering.sentence.Sentence(
-					actualSentence,
-					sentence[0],
-					sentenceNum,
-					None)
-			sentenceNum += 1
+			for extraction in ext.extractions():
+				if extraction.type == attensity.ExtractionMessage_pb2.Extraction.KEYWORD_RESULTS:
+					roots = {}
+					for item in extraction.keyword_results.root:
+						roots[item.id] = {"root": item.root, "word": item.word, "pos": item.pos}
+					for item in extraction.keyword_results.location:
+						roots[item.id]["sentence"] = item.sentence
+					for key in roots:
+						if "sentence" not in roots[key]:
+							continue
 
-		for keywordResult in newParagraph.extractionKeywordResults:
-			if keywordResult[0] in sentences:
-				sentences[keywordResult[0]].keywordResults.append(keywordResult)
-
-		for triple in newParagraph.extractionTriples:
-			sentences[triple[0]].triples.append(triple)
-
-		for entity in newParagraph.extractionEntities:
-			sentences[entity[0]].entities.append(entity)
-
-		for fact in newParagraph.extractionFacts:
-			sentences[fact[0]].facts.append(fact)
-
-		for phrase in newParagraph.extractionTextPhrases:
-			sentences[phrase[0]].phrases.append(phrase)
-
-		for sentence in sentences:
-			topicSummary.sentences.append(sentences[sentence])
+						try:
+							sentenceId = int(roots[key]["sentence"])
+							root = str(roots[key]["root"])
+							word = str(roots[key]["word"])
+							pos = list(roots[key]["pos"])
+							newParagraph.extractionKeywordResults.append((sentenceId, root, word, pos))
+						except Exception:
+							print "error happened"
+				if extraction.type == attensity.ExtractionMessage_pb2.Extraction.FACT_RELATION:
+					newParagraph.extractionFactRelations.append((extraction.fact_relation.fact_one, extraction.fact_relation.fact_two, extraction.fact_relation.text))
+				if extraction.type == attensity.ExtractionMessage_pb2.Extraction.TEXT_SENTENCE:
+					print "|||" + newParagraph.text[extraction.text_sentence.offset:extraction.text_sentence.offset + extraction.text_sentence.length] + "|||"
+					newParagraph.extractionSentences.append((extraction.text_sentence.text_sentence_ID, extraction.text_sentence.offset, extraction.text_sentence.length))
+				if extraction.type == attensity.ExtractionMessage_pb2.Extraction.ENTITY:
+					mid = ""
+					if len(extraction.entity.search_info) > 0:
+						mid = extraction.entity.search_info[0].machine_ID
+					newParagraph.extractionEntities.append((extraction.entity.sentence_id, extraction.entity.display_text, extraction.entity.sem_tags, extraction.entity.domain_role, mid))
+				if extraction.type == attensity.ExtractionMessage_pb2.Extraction.TRIPLE:
+					newParagraph.extractionTriples.append((extraction.triple.sentence_ID, extraction.triple.t1.value, extraction.triple.t1.sem_tags, extraction.triple.t2.value, extraction.triple.t2.sem_tags, extraction.triple.t3.value, extraction.triple.t3.sem_tags))
+					# print extraction
+				if extraction.type == attensity.ExtractionMessage_pb2.Extraction.FACT:
+					# print extraction
+					newParagraph.extractionFacts.append((extraction.fact.sentence_ID, extraction.fact.element.text, extraction.fact.mode.text))
+				if extraction.type == attensity.ExtractionMessage_pb2.Extraction.TEXT_PHRASE:
+					# print extraction
+					newParagraph.extractionTextPhrases.append((extraction.text_phrase.sentence_ID, extraction.text_phrase.head, extraction.text_phrase.root))
 
 		# cache
-		pickleFileName = os.path.join("../cache/goldCache", file)
+		pickleFileName = os.path.join("../cache/asasGoldCache", file)
 		pickleFile = open(pickleFileName, 'wb')
-		pickle.dump(topicSummary, pickleFile, pickle.HIGHEST_PROTOCOL)
+		pickle.dump(docModel, pickleFile, pickle.HIGHEST_PROTOCOL)
 		print "pickled " + pickleFileName
