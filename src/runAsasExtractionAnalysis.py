7cb7e4ada153c32e1f4f9cba9ffec586dbaec4e6
Mike Roylance <roylance.michael@gmail.com>
2015-05-25 16:21:56 -0600

    trigram word comparison with asas root keywords

--- src/runAsasExtractionAnalysis.py
+++ src/runAsasExtractionAnalysis.py
@@ -142,7 +142,7 @@ for fileName in os.listdir(cachePath):
 				for sentence in sentences:
 					sentences[sentence].assignEntityScores()
 					sentences[sentence].determineNounChunks()
-					sentences[sentence].createChunks(2)
+					sentences[sentence].createChunks(3)
 					allSentences[sentences[sentence].uniqueId] = sentences[sentence]
 
 		print "doing clustering now on summarization..."
