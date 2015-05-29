29593951728e40021cb7f446fa104ed993dde1e5
Brandon Gahler <gahlerbj@gmail.com>
2015-05-28 20:16:57 -0700

    added note to cAttencityFeatures

--- src/realize/cAttencityFeatures.py
+++ src/realize/cAttencityFeatures.py
@@ -16,4 +16,12 @@ for a in c_corpus:
     a.facts = []
     a.phrases = []
 
+    """
+    Ideally, I want to convert the above attencity outputs into word level features
+    a.features[ word_index ] [ "in_a_X_relation_in_a_triple" ] = True
+    sort of thing.
+    Brandon can try to handle that himself, but Mike, who understands better what these
+    are, would be better if he had time.
+    """
+
 pickle.dump(c_corpus, open(compressionCorpusCache+"c_Sentences_att_features", 'w'))
